17ddb411b89223aaa183c0b257e756c13a1253d4
helenaofs <lilklinik@yandex.ru>
2022-09-17 15:17:18 +0700

    Main method added

---
+++ Task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+string[] array = new string[4] {"Hi", "Hello", "Boy", "Girl"};
+string[] newArray = new string[array.Length];
+
+void MethodSortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
